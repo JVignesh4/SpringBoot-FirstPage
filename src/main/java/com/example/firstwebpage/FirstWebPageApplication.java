526727443ef54a2f0456b2343500f0a20e70c2f5
JVignesh4 <vigneshbuji04@gmail.com>
2022-09-09 17:25:54 +0530

    Added: Created First WebPage In Spring Boot

---
+++ src/main/java/com/example/firstwebpage/FirstWebPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstWebPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstwebpage/FirstWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstwebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
